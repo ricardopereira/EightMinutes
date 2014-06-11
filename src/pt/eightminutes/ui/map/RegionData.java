ccbeb76dec4f52c71fcebbe9d66b610522f9d041
Ricardo Pereira <ricardopereira.eu@gmail.com>
2014-06-11 21:41:20 +0100

    Closed #50, #57 - selecção da região no mapa

--- src/pt/eightminutes/ui/map/RegionData.java
+++ src/pt/eightminutes/ui/map/RegionData.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class RegionData implements IRegion, Serializable{
+public class RegionData implements IRegion, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
